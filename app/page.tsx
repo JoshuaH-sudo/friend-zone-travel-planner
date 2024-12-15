6d831a345e7abc47f4ee852eab07f7264da97ae0
Joshua Hoban <JHwork101@protonmail.com>
2024-12-15 09:39:09 +0100

    add custome css varaible stylesing to calander

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,9 @@
-"use client";
-import { useState } from "react";
-import { DayPicker } from "react-day-picker";
-import "react-day-picker/style.css";
+import Calender from "@/components/Calender";
 
 export default function Home() {
-  const [selected, setSelected] = useState<Date>();
   return (
-    <div className="flex h-screen items-center justify-center bg-slate-50">
-      <DayPicker
-        mode="single"
-        selected={selected}
-        onSelect={setSelected}
-        footer={
-          selected
-            ? `Selected: ${selected.toLocaleDateString()}`
-            : "Pick a day."
-        }
-      />
+    <div className="flex h-screen items-center justify-center">
+      <Calender />
     </div>
   );
 }
