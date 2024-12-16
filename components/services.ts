2d766fbebd34e92a91984643538abc17aa626cd7
JoshuaH-sudo <JHwork101@protonmail.com>
2024-12-16 16:29:20 +0100

    Enhance date range selection logic with improved checks and duplicate removal

--- components/services.ts
+++ components/services.ts
@@ -3,16 +3,21 @@ import { DateRangeNullable } from './types'
 import { isDayBefore, isDayInRange } from './DateUtils'
 
 export default class DatePickerRangeService {
+  // Determines if the user is selecting the first day of the range
   static isSelectingFirstDay = (
     from: Date | null,
     to: Date | null,
     day: Date
   ): boolean | Date | null => {
+    // Check if the selected day is before the first day
     const isBeforeFirstDay = from && isDayBefore(day, from)
+    // Check if the range is already selected
     const isRangeSelected = from && to
+    // Return true if no start date, or if the selected day is before the start date, or if the range is already selected
     return !from || isBeforeFirstDay || isRangeSelected
   }
 
+  // Increases the ranges that are smaller than the temporary range
   static increaseSmallerRanges = (
     tempRangeNullable: DateRangeNullable,
     ranges: DateRange[]
@@ -22,22 +27,28 @@ export default class DatePickerRangeService {
 
     const increasedRanges = ranges.map((r: DateRange) => {
       const { from, to } = r
+      // Check if the start date of the range is within the temporary range
       const isFromInTempRange = isDayInRange(from, tempRange)
+      // Check if the end date of the range is within the temporary range
       const isToInTempRange = isDayInRange(to, tempRange)
 
       if (isFromInTempRange && isToInTempRange) {
+        // If both start and end dates are within the temporary range, increase the range
         shouldIncrease = true
         return tempRange as DateRange
       } else if (isFromInTempRange && !isToInTempRange) {
+        // If only the start date is within the temporary range, adjust the range
         shouldIncrease = true
         return { from: tempRange.from, to }
       } else {
+        // Otherwise, return the original range
         return r
       }
     })
 
     return {
       shouldIncrease,
+      // Remove duplicate ranges
       increasedRanges: [...new Set(increasedRanges)]
     }
   }
