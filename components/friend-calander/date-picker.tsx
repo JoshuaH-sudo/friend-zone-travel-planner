0256c106eb324719061ce1ea1fd1750ec0e7243e
JoshuaH-sudo <jhwork101@protonmail.com>
2025-04-29 19:23:00 +0200

    feature/[fzt-10]refactor: remove unnecessary whitespace for cleaner code

--- components/friend-calander/date-picker.tsx
+++ components/friend-calander/date-picker.tsx
@@ -1,9 +1,7 @@
 'use client';
-
 import * as React from 'react';
 import { format } from 'date-fns';
 import { CalendarIcon } from 'lucide-react';
-
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import {
