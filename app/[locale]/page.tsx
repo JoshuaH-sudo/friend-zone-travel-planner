8484815bb9a52699749864def5fa63f0648139f4
JoshuaH-sudo <jhwork101@protonmail.com>
2025-05-17 17:19:08 +0200

    feature/[fzt-17]refactor: update shadow direction for feature frames and adjust footer styles

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -149,6 +149,7 @@ export default function LandingPage() {
             Icon={Users}
             imageSrc={productExampleRight}
             imageAlt='Friend Management example'
+            shadowDirection='right'
           />
         </div>
       </section>
@@ -180,12 +181,13 @@ export default function LandingPage() {
             Icon={FileClock}
             imageSrc={productExampleRight}
             imageAlt='Export to iCal example'
+            shadowDirection='right'
           />
         </div>
       </section>
 
-      <footer className='border-t bg-muted/30 py-8'>
-        <div className='container mx-auto px-4 text-center text-muted-foreground'>
+      <footer className='h-24 border-t bg-green-300 py-8'>
+        <div className='container mx-auto px-4 text-center text-black'>
           <a
             href='https://www.flaticon.com/free-icons/travel'
             title='travel icons'
@@ -221,41 +223,50 @@ export interface FeatureFrameProps {
   >;
   imageSrc: StaticImageData;
   imageAlt: string;
+  shadowDirection?: 'left' | 'right';
   className?: string;
 }
 
-const FeatureFrame: FC<FeatureFrameProps> = ({
+export const FeatureFrame: FC<FeatureFrameProps> = ({
   title,
   Icon,
   imageSrc,
   imageAlt,
+  shadowDirection = 'left',
   className,
-}) => (
-  <div
-    className={cn(
-      'inline-flex w-[472px] flex-col items-start justify-center gap-3 overflow-hidden rounded-xl bg-white px-6 py-6 shadow-[-12px_12px_4px_0px_rgba(0,0,0,0.25)]',
-      className
-    )}
-  >
-    <div className='flex h-72 flex-col items-start justify-start gap-4 self-stretch'>
-      <div className='inline-flex items-center justify-start gap-2.5'>
-        <div className='relative inline-flex flex-col items-start justify-between overflow-hidden rounded-full bg-black p-1 shadow-[-2px_2px_0px_0px_rgba(0,203,51,1.00)]'>
-          <Icon />
+}) => {
+  const leftShadowClassName = 'shadow-[-12px_12px_4px_0px_rgb(0_0_0_/_0.25)]';
+  const rightShadowClassName = 'shadow-[12px_12px_4px_0px_rgb(0_0_0_/_0.25)]';
+  const shadowClassName =
+    shadowDirection === 'left' ? leftShadowClassName : rightShadowClassName;
+  return (
+    <div
+      className={cn(
+        'inline-flex w-[472px] flex-col items-start justify-center gap-3 overflow-hidden rounded-xl bg-white px-6 py-8',
+        shadowClassName,
+        className
+      )}
+    >
+      <div className='flex h-72 flex-col items-start justify-start gap-4 self-stretch'>
+        <div className='inline-flex items-center justify-start gap-2.5'>
+          <div className='relative inline-flex flex-col items-start justify-between overflow-hidden rounded-full bg-black p-1 shadow-[-2px_2px_0px_0px_rgba(0,203,51,1.00)]'>
+            <Icon />
+          </div>
+          <div className="justify-start font-['Roboto'] text-3xl font-semibold leading-9 text-black">
+            {title}
+          </div>
         </div>
-        <div className="justify-start font-['Roboto'] text-3xl font-semibold leading-9 text-black">
-          {title}
-        </div>
+        <Image src={imageSrc} alt={imageAlt} width={717} height={428} />
       </div>
-      <Image src={imageSrc} alt={imageAlt} width={717} height={428} />
     </div>
-  </div>
-);
+  );
+};
 
 export interface FeatureTextProps extends PropsWithChildren {
   shadowDirection?: 'left' | 'right';
   className?: string;
 }
-const FeatureText: FC<FeatureTextProps> = ({
+export const FeatureText: FC<FeatureTextProps> = ({
   shadowDirection = 'left',
   className,
   children,
