b464b33b88ff76f6d4f995facdb3b3324a0f812d
JoshuaH-sudo <jhwork101@protonmail.com>
2025-05-18 09:54:02 +0200

    feature/[fzt-17]Refactor code structure for improved readability and maintainability

--- app/[locale]/components/FeatureFrame.tsx
+++ app/[locale]/components/FeatureFrame.tsx
@@ -36,14 +36,20 @@ const FeatureFrame: FC<FeatureFrameProps> = ({
     >
       <div className='flex h-72 flex-col items-start justify-start gap-4 self-stretch'>
         <div className='inline-flex items-center justify-start gap-2.5'>
-          <div className='relative text-white inline-flex flex-col items-start justify-between overflow-hidden rounded-full bg-black p-1 shadow-[-2px_2px_0px_0px_rgba(0,203,51,1.00)]'>
+          <div className='relative inline-flex flex-col items-start justify-between overflow-hidden rounded-full bg-black p-1 text-white shadow-[-2px_2px_0px_0px_rgba(0,203,51,1.00)]'>
             <Icon />
           </div>
           <div className="justify-start font-['Roboto'] text-3xl font-semibold leading-9 text-black">
             {title}
           </div>
         </div>
-        <Image src={imageSrc} alt={imageAlt} width={717} height={428} />
+        <Image
+          src={imageSrc}
+          alt={imageAlt}
+          width={717}
+          height={428}
+          className='rounded-xl'
+        />
       </div>
     </div>
   );
