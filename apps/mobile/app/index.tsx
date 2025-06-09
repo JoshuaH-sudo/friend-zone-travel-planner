6eb9c8655358b12fdcdab6ae87af6bb8b8bfd904
JoshuaH-sudo <jhwork101@protonmail.com>
2025-06-09 19:23:33 +0200

    fix: update WebView source URL for development and production environments

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -2,13 +2,15 @@ import { WebView } from "react-native-webview";
 import Constants from "expo-constants";
 import { StyleSheet } from "react-native";
 
+const isDevelopment = process.env.NODE_ENV === "development";
+
 export default function Index() {
   // https://github.com/react-native-webview/react-native-webview/blob/master/docs/Guide.md
   return (
     <WebView
       style={styles.container}
       showsHorizontalScrollIndicator={false}
-      source={{ uri: "http://localhost:3000" }}
+      source={{ uri: isDevelopment ? "http://localhost:3000" : "https://www.friend-zone.app/" }}
     />
   );
 }
