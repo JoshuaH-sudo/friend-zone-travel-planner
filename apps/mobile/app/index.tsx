312f72f86350c05ff6fc39c9cd108a330fcdc139
JoshuaH-sudo <jhwork101@protonmail.com>
2025-06-09 19:04:28 +0200

    fix: update WebView source URL to remove language path

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -3,11 +3,12 @@ import Constants from "expo-constants";
 import { StyleSheet } from "react-native";
 
 export default function Index() {
+  // https://github.com/react-native-webview/react-native-webview/blob/master/docs/Guide.md
   return (
     <WebView
       style={styles.container}
       showsHorizontalScrollIndicator={false}
-      source={{ uri: "http://localhost:3000/en" }}
+      source={{ uri: "http://localhost:3000" }}
     />
   );
 }
