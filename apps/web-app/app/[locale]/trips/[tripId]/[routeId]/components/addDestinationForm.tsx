de7f831fb3e6d0dfe6104b0b5394bb45d32e04cb
JoshuaH-sudo <jhwork101@protonmail.com>
2025-06-19 20:22:26 +0200

    feat: integrate Form component into AddDestinationForm and enhance form handling

--- apps/web-app/app/[locale]/trips/[tripId]/[routeId]/components/addDestinationForm.tsx
+++ apps/web-app/app/[locale]/trips/[tripId]/[routeId]/components/addDestinationForm.tsx
@@ -11,6 +11,7 @@ import { useQueryClient } from '@tanstack/react-query';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import {
+  Form,
   FormField,
   FormItem,
   FormLabel,
@@ -39,12 +40,7 @@ const AddDestinationForm: FC<AddDestinationFormProps> = ({
   currentOrder = 0,
 }) => {
   const queryClient = useQueryClient();
-  const {
-    control,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<NewDestination>({
+  const form = useForm<NewDestination>({
     defaultValues: {
       location: '',
       order: currentOrder + 1,
@@ -52,7 +48,13 @@ const AddDestinationForm: FC<AddDestinationFormProps> = ({
     },
     resolver: zodResolver(schema),
   });
-  console.log('errors:', errors);
+
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { isValid },
+  } = form;
   const { mutateAsync: addDestinationToRoute } = useAddDestinationToRoute({
     onSuccess: () => {
       reset();
@@ -72,57 +74,58 @@ const AddDestinationForm: FC<AddDestinationFormProps> = ({
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className='flex h-full flex-col gap-1'
-    >
-      <div>
-        <p>Destination</p>
-        <FormField
-          control={control}
-          name='location'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel />
-              <FormControl>
-                <Input placeholder={'Berlin'} {...field}/>
-              </FormControl>
-              <FormDescription />
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-      </div>
-      <p>{JSON.stringify(errors)}</p>
-
-      <div>
-        <p>Dates</p>
-        <DatePickerWithRange />
-      </div>
-
-      <div className='h-1/3'>
-        <p>Friends To See</p>
-        <ScrollArea>
-          {friends.map((friend) => (
-            <div
-              key={friend.id}
-              className='my-2 cursor-pointer rounded-lg bg-gray-200 p-2 text-black transition-colors duration-200 hover:bg-red-400'
-            >
-              {friend.name}
-            </div>
-          ))}
-        </ScrollArea>
-      </div>
-
-      <button
-        id='add-destination'
-        className='w-full rounded-lg bg-green-500 p-2 text-white transition-colors duration-200 hover:bg-green-600 disabled:opacity-50'
-        type='submit'
-        disabled={!!errors}
+    <Form {...form}>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='flex h-full flex-col gap-1'
       >
-        Add
-      </button>
-    </form>
+        <div>
+          <p>Destination</p>
+          <FormField
+            control={control}
+            name='location'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Location</FormLabel>
+                <FormControl>
+                  <Input placeholder='Berlin' {...field} />
+                </FormControl>
+                <FormDescription>Enter your destination</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div>
+          <p>Dates</p>
+          <DatePickerWithRange />
+        </div>
+
+        <div className='h-1/3'>
+          <p>Friends To See</p>
+          <ScrollArea>
+            {friends.map((friend) => (
+              <div
+                key={friend.id}
+                className='my-2 cursor-pointer rounded-lg bg-gray-200 p-2 text-black transition-colors duration-200 hover:bg-red-400'
+              >
+                {friend.name}
+              </div>
+            ))}
+          </ScrollArea>
+        </div>
+
+        <button
+          id='add-destination'
+          className='w-full rounded-lg bg-green-500 p-2 text-white transition-colors duration-200 hover:bg-green-600 disabled:opacity-50'
+          type='submit'
+          disabled={!isValid}
+        >
+          Add
+        </button>
+      </form>
+    </Form>
   );
 };
 
