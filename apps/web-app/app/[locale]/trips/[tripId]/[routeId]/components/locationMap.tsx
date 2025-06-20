b410ff4085bf43f0359f35244c356a071c07cd14
JoshuaH-sudo <jhwork101@protonmail.com>
2025-06-20 17:28:35 +0200

    feat: update LocationMap to fetch destinations by routeId and use user geolocation

--- apps/web-app/app/[locale]/trips/[tripId]/[routeId]/components/locationMap.tsx
+++ apps/web-app/app/[locale]/trips/[tripId]/[routeId]/components/locationMap.tsx
@@ -1,40 +1,54 @@
 'use client';
+import useGetDestinationsByRouteId from '@/lib/hooks/useGetDestinationsByRouteId';
 import {
   AdvancedMarker,
   APIProvider,
   Map,
   Pin,
 } from '@vis.gl/react-google-maps';
+import { set } from 'date-fns';
+import { FC, useEffect, useState } from 'react';
 
-type Poi = { key: string; location: google.maps.LatLngLiteral };
-const locations: Poi[] = [
-  { key: 'operaHouse', location: { lat: -33.8567844, lng: 151.213108 } },
-  { key: 'tarongaZoo', location: { lat: -33.8472767, lng: 151.2188164 } },
-  { key: 'manlyBeach', location: { lat: -33.8209738, lng: 151.2563253 } },
-  { key: 'hyderPark', location: { lat: -33.8690081, lng: 151.2052393 } },
-  { key: 'theRocks', location: { lat: -33.8587568, lng: 151.2058246 } },
-  { key: 'circularQuay', location: { lat: -33.858761, lng: 151.2055688 } },
-  { key: 'harbourBridge', location: { lat: -33.852228, lng: 151.2038374 } },
-  { key: 'kingsCross', location: { lat: -33.8737375, lng: 151.222569 } },
-  { key: 'botanicGardens', location: { lat: -33.864167, lng: 151.216387 } },
-  { key: 'museumOfSydney', location: { lat: -33.8636005, lng: 151.2092542 } },
-  { key: 'maritimeMuseum', location: { lat: -33.869395, lng: 151.198648 } },
-  { key: 'kingStreetWharf', location: { lat: -33.8665445, lng: 151.1989808 } },
-  { key: 'aquarium', location: { lat: -33.869627, lng: 151.202146 } },
-  { key: 'darlingHarbour', location: { lat: -33.87488, lng: 151.1987113 } },
-  { key: 'barangaroo', location: { lat: -33.8605523, lng: 151.1972205 } },
-];
-const LocationMap = () => {
-  console.log(
-    'Google Maps API Key:',
-    process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
-  );
+export type Poi = { key: string; location: google.maps.LatLngLiteral };
+
+interface LocationMapProps {
+  routeId: number;
+}
+const LocationMap: FC<LocationMapProps> = ({ routeId }) => {
+  const { data: destinations } = useGetDestinationsByRouteId(routeId);
+  const locations: Poi[] =
+    destinations?.map((destination) => ({
+      key: destination.location,
+      location: {
+        lat: destination?.latitude || 0,
+        lng: destination?.longitude || 0,
+      },
+    })) || [];
+
+  const [userLocation, setUserLocation] = useState<google.maps.LatLngLiteral>({
+    lat: 0,
+    lng: 0,
+  });
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) =>
+        setUserLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        }),
+      () => setUserLocation({ lat: 0, lng: 0 }) // Fallback if geolocation fails
+    );
+  }, []);
+
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
       <Map
+        // Force re-render when user location changes
+        key={userLocation.lat + userLocation.lng}
         mapId='e8e51ecff87a146cf2857bda'
         style={{ width: '100%', height: '100%' }}
-        defaultCenter={locations[0].location}
+        defaultCenter={userLocation}
         defaultZoom={13}
         gestureHandling={'greedy'}
         disableDefaultUI={true}
