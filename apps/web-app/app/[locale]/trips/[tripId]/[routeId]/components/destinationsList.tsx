296c201278117a07b192c242cb5f6a8d95305f13
JoshuaH-sudo <jhwork101@protonmail.com>
2025-06-24 18:02:24 +0200

    feat: restructure DestinationList component layout for improved readability

--- apps/web-app/app/[locale]/trips/[tripId]/[routeId]/components/destinationsList.tsx
+++ apps/web-app/app/[locale]/trips/[tripId]/[routeId]/components/destinationsList.tsx
@@ -16,8 +16,14 @@ const DestinationList: FC<DestinationListProps> = ({ routeId }) => {
       <ScrollArea className='h-full'>
         {destinations?.map((destination, index) => (
           <div key={destination.id} className='flex flex-col items-center'>
-            <div className='cursor-pointer rounded-lg bg-gray-200 p-2 text-black transition-colors duration-200 hover:bg-red-400'>
-              {destination.location}
+            <div className='flex flex-row items-center gap-2 w-full'>
+              <div className='flex size-8 items-center justify-center rounded-full bg-gray-200 text-black'>
+                {index + 1}
+              </div>
+
+              <div className='cursor-pointer rounded-lg bg-gray-200 p-2 px-4 text-black transition-colors duration-200 hover:bg-red-400 flex-1'>
+                {destination.location}
+              </div>
             </div>
             {index < destinations.length - 1 && (
               <ChevronDown
