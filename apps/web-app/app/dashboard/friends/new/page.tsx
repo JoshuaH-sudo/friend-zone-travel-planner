9628b2020cd6f71d6b23309a702d90905667be58
JoshuaH-sudo <jhwork101@protonmail.com>
2025-07-27 20:08:04 +0200

    feature/[fzt-28] feat: restructure friend management components and actions for new friend addition

--- apps/web-app/app/dashboard/friends/new/page.tsx
+++ apps/web-app/app/dashboard/friends/new/page.tsx
@@ -3,7 +3,7 @@
 import { useRouter } from 'next/navigation'
 import { Button } from '@/components/ui/button'
 import { ArrowLeft } from 'lucide-react'
-import AddFriendForm from '../components/AddFriendForm'
+import AddFriendForm from './components/AddFriendForm'
 
 const NewFriendPage = () => {
   const router = useRouter()
